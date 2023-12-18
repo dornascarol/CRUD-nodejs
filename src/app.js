34a46749d72945b38242795eb6ce7c101979e48f
dornascarol <carolinafdornas@gmail.com>
2023-12-17 22:31:16 -0300

    atualiza os arquivos de app e server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 //TODO: express, config swagger, dotenv cors, json, mongoose.Connect e usar rotas
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+module.exports = app
