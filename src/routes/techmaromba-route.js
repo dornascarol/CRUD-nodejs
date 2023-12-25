1fa3265a74df40b3faa1729a868f0e61e96f7ec9
dornascarol <carolinafdornas@gmail.com>
2023-12-25 20:55:04 -0300

    cria rotas get, post, patch e delete

--- src/routes/techmaromba-route.js
+++ src/routes/techmaromba-route.js
@@ -1,1 +1,14 @@
 //TODO: Importar o express, o controller e instanciar o Router para chamar os metodos HTTP passando o endereço e função do CRUD referente 
+const express = require("express")
+
+const router = express.Router()
+
+router.get("/all", findAll)
+
+router.post("/add", addNew)
+
+router.patch("/update/:id", update)
+
+router.delete("/clear/:id", clear)
+
+module.exports = router
