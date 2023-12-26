8c2a80bf70ac7853508d092696ffd38ebcf4eddb
dornascarol <carolinafdornas@gmail.com>
2023-12-25 21:43:05 -0300

    liga as rotas no app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 //TODO: express, config swagger, dotenv cors, json, mongoose.Connect e usar rotas
 const express = require("express")
 const cors = require("cors")
-
+const techMarombaRoute = require("./routes/techmaromba-route")
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use("/techmaromba", techMarombaRoute)
 
 module.exports = app
