d24ed11043b129761ba0ebfba8cc1829e2b59288
dornascarol <carolinafdornas@gmail.com>
2024-01-04 23:12:22 -0300

    configura para integrar o MongoDB e importa para o app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 //TODO: express, config swagger, dotenv cors, json, mongoose.Connect e usar rotas
+
 const express = require("express")
 const cors = require("cors")
 const techMarombaRoute = require("./routes/techmaromba-route")
+const mongoose = require("./database/dbConnect")
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+mongoose.connect()
 app.use("/techmaromba", techMarombaRoute)
 
 module.exports = app
--- src/database/dbConnect.js
+++ src/database/dbConnect.js
@@ -1,1 +1,17 @@
 //TODO: Inserir configurações para integrar o MongoDB
+
+const mongoose = require("mongoose")
+
+const connect = async () => {
+    try {
+        mongoose.connect("mongodb+srv://carolinafdornas:G0CCTltK25nJxesy@cluster0.aqjkj2d.mongodb.net/", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("Banco de dados conectado")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = { connect } 
